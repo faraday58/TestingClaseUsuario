7c17f29d530a8b82ba455e9c1e83022610dfee33
armando <jarodriguezvera@gmail.com>
2024-04-01 12:45:27 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UsuarioLibrary;
+
+namespace TestingClaseUsuario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Usuario miUsuario = new Usuario("", "");
+
+            Console.WriteLine("nombre: {0} \n password: {1}",
+                miUsuario.Nombre, 
+                miUsuario.Password
+                );
+            Console.ReadLine();
+        }
+    }
+}
